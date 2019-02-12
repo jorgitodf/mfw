5a64a7906cd5140bc4d7fd00a685aa680d86edfd
Jorgito Paiva <jspaiva.1977@gmail.com>
2019-02-12 19:49:06 -0200

    Alteração 12/02/2019

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -19,18 +19,18 @@ class HomeController
     {
         $user = Password::getSessionUser();
         $idConta = Password::hasConta();
-
+        
         $contas = $c[$this->getModel('ac')]->getAllById(['fk_users' => $user['idLoggedIn']]);
 
         if ($idConta) {
             $pgtos = $c[$this->getModel('sp')]->between(['fk_account' => $idConta], 'data_pagamento', ano().'-'.verificaMesNumerico().'-01', ano().'-'.verificaMesNumerico().'-31');
         }
-
+ 
         if (!$contas) {
             $msg = "Prezado(a) {$user['name']}, você não possui nenhuma Conta Ativa ou Cadastrada.";
             $msg .= " Para acesso às funcionalidades do Sistema, favor cadastre uma Conta!";
         }
-        
+    
         return $c['view']->render('home/index.html.twig', ['title' => 'Página Home', 'data' => $msg, 'contas' => $contas, 'pgtos' => $pgtos]);
     }
 }
